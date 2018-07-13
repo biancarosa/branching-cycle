63d11065c2c7eea9b325edfc19a134d4fcbe78ef
Bianca Rosa <me@biancarosa.com.br>
2018-07-13 11:01:24 -0300

    fix fucking stupid mistake with routing methods

--- branching_cycle/webhook/blueprint.py
+++ branching_cycle/webhook/blueprint.py
@@ -5,5 +5,5 @@ from branching_cycle.webhook import webhook
 def create_blueprint():
     """Creates a Blueprint"""
     blueprint = Blueprint('Webhook Blueprint', __name__)
-    blueprint.route('/git-webhook')(webhook.git_webhook)
+    blueprint.route('/git-webhook', methods=['POST', 'OPTIONS'])(webhook.git_webhook)
     return blueprint
