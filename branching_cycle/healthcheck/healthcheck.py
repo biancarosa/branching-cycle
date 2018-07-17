dcb00802beb953ac16981f7f9cc31b4f220d5fc4
Bianca Rosa <me@biancarosa.com.br>
2018-07-17 12:12:22 -0300

    add service healthcheck

--- branching_cycle/healthcheck/healthcheck.py
+++ branching_cycle/healthcheck/healthcheck.py
@@ -1,9 +1,21 @@
 """Deals with HealthCheck route."""
 from flask import jsonify
+from pymongo import MongoClient
+import os
 
 def healthcheck():
     """Returns health information"""
+    status = {
+        "mongodb" : True
+    }
+    try:
+        client = MongoClient(os.getenv('MONGODB_URI', 'mongodb://localhost:27017'))
+    except Exception:
+        status["mongodb"] = False
     return jsonify({
-        "message": "I feel good."
+        "message": "I feel good.",
+        "services": {
+            "mongodb": status["mongodb"]
+        }
     })
     
