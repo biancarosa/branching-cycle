5fcb181ddad54f94fec72c95ab0b1e35b88ea562
Bianca Rosa <me@biancarosa.com.br>
2018-07-17 12:28:38 -0300

    insert data in dabase

--- branching_cycle/webhook/webhook.py
+++ branching_cycle/webhook/webhook.py
@@ -1,13 +1,24 @@
 """Deals with WebHook route."""
+import os
+from datetime import date
+
 from flask import jsonify, request
+from pymongo import MongoClient
 from branching_cycle import logger
 
 log = logger.getLogger(__name__)
 
 def git_webhook():
-    """Returns okay!"""
-    content = request.json
+    """Returns okay and saves in the DB!"""
+    client = MongoClient(os.getenv('MONGODB_URI', 'mongodb://localhost:27017'))
+    database = client.github
+    content = {
+        "event": request.headers['X-GitHub-Event'],
+        "payload" : request.json,
+        "date": datetime.datetime.utcnow()
+    }
     log.info(content)
+    log.info("Content Inserted - ", database.events.insert_one(content).inserted_id)
     return jsonify({
         "message": "Okay!"
     })
